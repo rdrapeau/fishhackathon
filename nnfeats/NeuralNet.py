4ba9a86972ece99f7cb4240267a8ab1b3e1cdbdf
Aaron Nech <aaronnech@gmail.com>
2016-04-22 20:17:12 -0700

    fixed

---
+++ nnfeats/NeuralNet.py
@@ -0,0 +1,36 @@
+import atexit
+import binascii
+from subprocess import Popen, PIPE
+import os
+import os.path
+import sysimport numpy as np
+import cv2
+
+myDir = os.path.dirname(os.path.realpath(__file__))
+os.environ['TERM'] = 'linux'
+
+class NeuralNet:
+    def __init__(self, model, imgDim=96, cuda=False, gpu=1):
+        assert model is not None
+        assert imgDim is not None
+        assert cuda is not None
+        assert gpu is not None
+
+        self.cmd = ['th', os.path.join(myDir, 'torch_server.lua'),
+                    '-model', model, '-imgDim', str(imgDim), '-gpuSelect', str(gpu)]
+        if cuda:
+            self.cmd.append('-cuda')
+        self.p = Popen(self.cmd, stdin=PIPE, stdout=PIPE,
+                       stderr=PIPE, bufsize=0)
+
+        def exitHandler():
+            if self.p.poll() is None:
+                self.p.kill()
+        atexit.register(exitHandler)
+
+    def forwardPath(self, imgPath):
+        assert imgPath is not None
+
+        rc = self.p.poll()
+        if rc is not None and rc != 0:
+            raise Exception("""
---
+++ nnfeats/torch_server.lua
@@ -0,0 +1,58 @@
+#!/usr/bin/env th
+
+require 'torch'
+require 'nn'
+require 'dpnn'
+require 'image'
+require 'loadcaffe'
+
+io.stdout:setvbuf 'no'
+torch.setdefaulttensortype('torch.FloatTensor')
+
+local cmd = torch.CmdLine()
+cmd:text('Options:')
+
+cmd:option('-model', '', 'Path to model.')
+cmd:option('-imgDim', 96, 'Image dimension. nn1=224, nn4=96')
+cmd:option('-cuda', false)
+cmd:option('-gpuSelect', 1)
+cmd:text()
+
+opt = cmd:parse(arg or {})
+
+net = loadcaffe.load('vgg19.prototxt', 'vgg19.caffemodel', 'nn')
+net:evaluate()
+
+local imgCuda = nil
+if opt.cuda then
+   require 'cutorch'
+   require 'cunn'
+   cutorch.setDevice(opt.gpuSelect)
+   net = net:cuda()
+   imgCuda = torch.CudaTensor(1, 3, opt.imgDim, opt.imgDim)
+end
+
+local img = torch.Tensor(1, 3, opt.imgDim, opt.imgDim)
+while true do
+   local imgPath = io.read("*line")
+   if imgPath and imgPath:len() ~= 0 then
+      img[1] = image.load(imgPath, 3, byte)
+      img[1] = image.scale(img[1], opt.imgDim, opt.imgDim)
+      local rep
+      if opt.cuda then
+         imgCuda:copy(img)
+         rep = net:forward(imgCuda):float()
+      else
+         rep = net:forward(img)
+      end
+      local sz = rep:size(1)
+      for i = 1,sz do
+         io.write(rep[i])
+         if i < sz then
+            io.write(',')
+         end
+      end
+      io.write('\n')
+      io.stdout:flush()
+   end
+end
